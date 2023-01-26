39e0d4288f63af1a62b67f232609de7680297863
henok8515 <henokegezew44@gmail.com>
2023-01-27 00:19:28 +0300

    cicd

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -29,7 +29,7 @@ export default function Intro() {
               fontSize: "120px",
             }}
           >
-            <span>  I'M Henok</span>
+            <span>  I'M Gelete</span>
           
           </h2>
           <h1
