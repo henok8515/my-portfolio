3432f37f51671ee8d56c3d6765026ec8ec51a5cc
Your Name <you@example.com>
2024-02-05 14:57:44 +0300

    fixing the the width problem

--- src/Components/AboutME/About.jsx
+++ src/Components/AboutME/About.jsx
@@ -12,16 +12,18 @@ function About() {
           <h1>
             <span>About Me</span>
           </h1>
-          Hi, I'm Henok. A Software Engineer/React Developer with
-          <span> 1+ years</span> of professional experience developing and
-          maintaining web applications. I have a passion for front-end
-          development. I specialize in<span> Javascript</span>,
-          <span> React</span>
-          Over the past year, I was fortunate enough to work with clients all
-          around the world across different industries helping them build
-          scalable apps. I pride myself on the quality of my work and my
-          eagerness to learn. My goal is to create as many happy customers and
-          learn as much from them in this ever-advancing field.
+          <h2 className="h2">
+            Hi, I'm Henok. A Software Engineer/React Developer with
+            <span> 1+ years</span> of professional experience developing and
+            maintaining web applications. I have a passion for front-end
+            development. I specialize in<span> Javascript</span>,
+            <span> React</span>
+            Over the past year, I was fortunate enough to work with clients all
+            around the world across different industries helping them build
+            scalable apps. I pride myself on the quality of my work and my
+            eagerness to learn. My goal is to create as many happy customers and
+            learn as much from them in this ever-advancing field.
+          </h2>
           <Button
             disable
             id="button"
